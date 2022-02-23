5fbb116fa710addb5f872ba24c1460d9037c85ad
Joel Mataway <jmataway@yahoo.com>
2022-02-22 17:43:54 -0700

    update defaults

--- src/runLicenseCheck.ts
+++ src/runLicenseCheck.ts
@@ -35,8 +35,7 @@ export async function runLicenseCheck({
       'license-checker',
       '--production',
       '--json',
-      `--failOn=${failLicenses},
-      --excludePackages='prince-backend@0.0.0;prince-mobile@1.0.14;prince-dev-tools@0.0.5;prince-sdk@2.2.0;prince-sdk@2.1.0;cli-color@0.1.7'`
+      `--failOn='${failLicenses}'`
     ],
     options
   );
